a819ba559c3baa41e00a2d57a56fa9832f1da194
Julio Cesar <julio.cdol1@gmail.com>
2023-11-16 15:20:53 -0300

    Adicionando Client + Server e implementando fármacia

--- server/index.js
+++ server/index.js
@@ -9,22 +9,39 @@ app.use(express.json());
 const db = mysql.createPool({
     host: "localhost",
     user: "root",
-    password: "root123",
+    password: "123123",
     database: "bditens",
 })
 
-app.listen(3001, () => {
+
+app.post("/Favorito", (req, res) => {
+    const { nome, imagem } = req.body;
+    let SQL = "INSERT INTO Favorito ( nome, imagem ) VALUES (?, ?)";
+    console.log(nome, imagem)
+    db.query(SQL, [ nome, imagem ] , (err, result) => {
+        console.log(err);
+        console.log(result);
+    })
+})
+
+app.get("/Favorito", (req, res) => {
+    let sql = "SELECT * FROM Favorito";
+    db.query(sql , (err, result) => {
+        res.send(result);
+    })
+})
+
+app.delete("/item/:id_favorito", (req, res) => {
+    const { id_favorito } = req.params;
+    console.log("Informação: ", id_favorito) 
+
+    let SQL = "DELETE FROM Favorito WHERE (`id_favorito` = ?)"
+
+    db.query(SQL, id_favorito, (err, result) => {
+        console.log(err)
+    })
+})
+
+app.listen(3006, () => {
     console.log("rodando servidor");
 });
-
-app.post("/item", (req, res) =>{
-    const { item } = req.body;
-    let SQL = "INSERT INTO cadastro (itens) VALUES ('cadastrar usuário')";
-    db.query(SQL, item, (err, result) => {
-        console.log(err);
-    })
-}
-
-
-
-);
